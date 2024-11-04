33502497e79367de2b9b345c2eccab9c77b24e09
Nanami <royalpankaj63@gmail.com>
2024-11-04 09:56:47 +0530

    context provider in client page to all server component

--- app/client-route/page.js
+++ app/client-route/page.js
@@ -3,30 +3,20 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { useTheme } from '@/components/themeProvider';
 
 
 
 
 export default function ClientRoutePage() {
-    const settings = {
+
+  const theme=useTheme()
+  const settings = {
         dots: true,
       };
       return (
-        <div className="image-slider-container">
-          <Slider {...settings}>
-          <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-          </Slider>
-        </div>
+       <h1 style={{
+        color:theme.colors.primary
+       }}> Client route</h1>
       );
     }
